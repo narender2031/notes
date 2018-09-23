e1d2264c0615c1e9f8cf3ca2878f76c7bdf62232
narender <narender2031@gmail.com>
2018-09-23 17:24:07 +0530

    update the code with all function

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -33,7 +33,6 @@ class NoteController < ApplicationController
             if h_tag.start_with? "#"
                 @tag = HashTag.find_by(hash_tag: h_tag)
                 if !@tag.present?
-                    h_tag.slice! "#"
                     @note.hash_tags.create(:hash_tag => h_tag)
                 else
                     @note.hash_tags_notes.create(:note_id=> @note.id, hash_tag_id: @tag.id)
@@ -59,24 +58,36 @@ class NoteController < ApplicationController
         if @note.present?
             @note.hash_tags_notes.delete_all
             @tags = []
+            @count = []
             if @note.update(note_params)
                 @hash_tag = note_params[:note].split()
                 @hash_tag.each do |tag|
                     if tag.start_with? "#"
                         @tag = HashTag.find_by(hash_tag: tag)
                         if !@tag.present?
-                            tag.slice! "#"
                             @note.hash_tags.create(:hash_tag => tag)
                         else
                             @note.hash_tags_notes.create(:note_id=> @note.id, hash_tag_id: @tag.id)
                         end
                     end
                 end
-                if HashTagsNote.present?
-                    HashTagsNote.order(updated_at: :desc).pluck(:hash_tag_id).each do |a|
-                        if !@tags.include?(id: a)
-                            @tags.push(HashTag.find(a))
+                if HashTagsNote.count != 0
+                    HashTagsNote.all.order(created_at: "desc").group_by(&:updated_at).values.each do |a|
+                        if a.length == 1
+                            puts "Hello"
+                            @tags.push(HashTag.find(a[0].hash_tag_id))
+                        else
+                            temp = 0
+                          a.each do |hash_tag|
+                            count =  HashTagsNote.where(hash_tag_id: hash_tag.hash_tag_id).count
+                            @count.push({id: hash_tag.hash_tag_id, count: count})
+                          end
                         end
+                    end
+                    @count.uniq! {|u| u[:id]}
+                    @sorted = @count.sort_by { |hsh| hsh[:count] }.reverse
+                    @sorted.each do |hash|
+                        @tags.push(HashTag.find(hash[:id]))
                     end
                 end
                 respond_to do |format|
@@ -89,11 +100,24 @@ class NoteController < ApplicationController
     def destroy
         @note = Note.find(params[:id])
         @tags = []
+        @count = []
         if HashTagsNote.count != 0
-            HashTagsNote.order(updated_at: :desc).pluck(:hash_tag_id).each do |a|
-                if !@tags.include?(id: a)
-                    @tags.push(HashTag.find(a))
+            HashTagsNote.all.order(created_at: "desc").group_by(&:updated_at).values.each do |a|
+                if a.length == 1
+                    puts "Hello"
+                    @tags.push(HashTag.find(a[0].hash_tag_id))
+                else
+                    temp = 0
+                  a.each do |hash_tag|
+                    count =  HashTagsNote.where(hash_tag_id: hash_tag.hash_tag_id).count
+                    @count.push({id: hash_tag.hash_tag_id, count: count})
+                  end
                 end
+            end
+            @count.uniq! {|u| u[:id]}
+            @sorted = @count.sort_by { |hsh| hsh[:count] }.reverse
+            @sorted.each do |hash|
+                @tags.push(HashTag.find(hash[:id]))
             end
         end
         if @note.present?
